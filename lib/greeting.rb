b38a819d646ce0802ea6ad4b6fce93d11c6c1d83
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-27 02:26:34 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,9 @@
 # code the #greeting method here!
 
 def greeting(name)
+<<<<<<< HEAD
   print "Hello #{name.strip}. It's nice to meet you."
+=======
+  puts "Hello #{name}\t
+>>>>>>> b5c259cae1d42b9df7925be4a5dccec9b80b2675
 end
